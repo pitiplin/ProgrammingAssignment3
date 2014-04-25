d4e5c240ab72d10650545ed131f5efc6f1fd7f69
cjgarcia <cjgarcia@indra.es>
2014-04-25 11:53:57 +0200

    run_analysis.R script is now well structured

--- run_analysis.R
+++ run_analysis.R
@@ -1,28 +1,66 @@
-#Part 1
-trainDataSet<-read.table("./UCI HAR Dataset/train/X_train.txt")
-testDataSet<-read.table("./UCI HAR Dataset/test/X_test.txt")
-mergedDataSet<-rbind(trainDataSet, testDataSet)
-labelsDataSetNames<-as.character(read.table("./UCI HAR Dataset/features.txt")[[2]])
-names(mergedDataSet) <- labelsDataSetNames
+Part1 <- function(trainfFile="./UCI HAR Dataset/train/X_train.txt", testFile="./UCI HAR Dataset/test/X_test.txt", labelsFile="./UCI HAR Dataset/features.txt"){
 
-#Part 2
-mergedDataSet<-mergedDataSet[c(grep("-mean()",names(mergedDataSet),fixed=T), grep("-std()",names(mergedDataSet),fixed=T))]
+				trainDataSet<-read.table(trainfFile)
+				testDataSet<-read.table(testFile)
+				mergedDataSet<-rbind(trainDataSet, testDataSet)
+				labelsDataSetNames<-as.character(read.table(labelsFile)[[2]])
+				names(mergedDataSet) <- labelsDataSetNames
+				
+				mergedDataSet
+}
 
-#Part 3
-trainActivity<-as.factor(readLines("./UCI HAR Dataset/train/y_train.txt"))
-testActivity<-as.factor(readLines("./UCI HAR Dataset/test/y_test.txt"))
-mergedActivity<-as.factor(c(trainActivity, testActivity))
+Part2 <- function(mergedDataSet){
 
-descriptiveActivityNames<-read.table("./UCI HAR Dataset/activity_labels.txt")
-levels(mergedActivity)<-descriptiveActivityNames[[2]]
+				mergedDataSet<-mergedDataSet[c(grep("-mean()",names(mergedDataSet),fixed=T), grep("-std()",names(mergedDataSet),fixed=T))]
+				
+				mergedDataSet
+}
 
-#Part 4
-mergedDataSet["Activity"] <- mergedActivity
+Part3 <- function(trainFile="./UCI HAR Dataset/train/y_train.txt", testFile="./UCI HAR Dataset/test/y_test.txt", labelsFile="./UCI HAR Dataset/activity_labels.txt"){
 
-#Part 5
-trainSubject<-as.factor(readLines("./UCI HAR Dataset/train/subject_train.txt"))
-testSubject<-as.factor(readLines("./UCI HAR Dataset/test/subject_test.txt"))
-mergedSubject<-as.factor(c(trainSubject, testSubject))
-mergedDataSet["Subject"] <- mergedSubject
+				trainActivity<-as.factor(readLines(trainFile))
+				testActivity<-as.factor(readLines(testFile))
+				mergedActivity<-as.factor(c(trainActivity, testActivity))
+				
+				descriptiveActivityNames<-read.table(labelsFile)
+				levels(mergedActivity)<-descriptiveActivityNames[[2]]
+				
+				mergedActivity
+}
 
-newDataSet<-t(as.data.frame(lapply(split(mergedDataSet, interaction(mergedDataSet$Activity,mergedDataSet$Subject)), function(x) colMeans(x[, 1:(ncol(x)-2)]))))
+Part4 <- function(mergedDataSet, mergedActivity){
+
+				mergedDataSet["Activity"] <- mergedActivity
+				
+				mergedDataSet
+}
+
+Part5 <- function(mergedDataSet, trainFile="./UCI HAR Dataset/train/subject_train.txt", testFile="./UCI HAR Dataset/test/subject_test.txt"){
+				
+				trainSubject<-as.factor(readLines(trainFile))
+				testSubject<-as.factor(readLines(testFile))
+				mergedSubject<-as.factor(c(trainSubject, testSubject))
+				mergedDataSet["Subject"] <- mergedSubject
+				
+				newDataSet<-t(as.data.frame(lapply(split(mergedDataSet, interaction(mergedDataSet$Activity,mergedDataSet$Subject)), function(x) colMeans(x[, 1:(ncol(x)-2)]))))
+}
+
+ExportDataSet <- function(newDataSet, path="DATA", textFileName="DataSetTXT", csvFileName="DataSetCSV"){
+
+				if (!file.exists(path)){
+								dir.create(path)
+				}
+				
+				write.table(newDataSet, paste0(path, "/", textFileName, ".txt"))
+				write.csv(newDataSet, paste0(path, "/", csvFileName, ".csv"))
+}
+
+GettingAndCleaningDataPeerAssessment <- function(){	
+
+				mergedDataSet <- Part1()
+				mergedDataSet <- Part2(mergedDataSet)
+				mergedActivity <- Part3()
+				mergedDataSet <- Part4(mergedDataSet, mergedActivity)
+				newDataSet <- Part5(mergedDataSet)
+				ExportDataSet(newDataSet)
+}
